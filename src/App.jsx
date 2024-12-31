492ff7c9a3cf124b7a607d4fe35cd88415a17165
Jacksen Nillson <nillsonjacksen@gmail.com>
2024-12-31 16:05:01 +0800

    add unique key prop to all entry components created by entryComponents inside App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,11 @@ import data from "./data"
 import './App.css'
 
 function App() {
+  // Creates an array of Entry components based on mapping over the data in data.js
   const entryComponents = data.map((entry) => {
     return (
       <Entry
+        key={entry.id}
         id={entry.id}
         img={entry.img}
         title={entry.title}
